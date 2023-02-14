65278ebc8f76a5ec4bd197a2ba237cb6784b6350
Michael <keliikalani87@gmail.com>
2023-02-14 13:11:55 -0800

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"We are adding {amount, 0:c} to your accaount");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        //public void Withdraw(double subtract) 
+        //{
+        //    Console.WriteLine($"We are subtracting {subtract, 0:c} from your account");
+        //}
+
+        
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much mullah wouuld you like to deposit?");
+            var userDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(userDeposit);
+
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your balance is: {userBalance, 0:c}");
+
         }
     }
 }
